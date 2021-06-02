2d223058f12a24eba4c78d39e719cf213b29727b
VijayGoswami007 <vrgoswami007@gmail.com>
2021-06-02 15:22:58 +0530

    initial commit

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,4 @@
+<div class="panel1 panel1-default">
+<br>
+<div class="panel-footer">Panel Footer</div>
+</div>
---
+++ resources/views/login.blade.php
@@ -0,0 +1,22 @@
+
+@extends('master')
+@section('content')
+<div class="container custom-login">
+    <div class="row">
+        <div class="col-sm-4 col-sm-offset-4">
+        <form>
+        <div class="form-group">
+            <label for="exampleInputEmail1" >Email address</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter Email" aria-describedby="emailHelp">
+        </div>
+        <div class="form-group">
+            <label for="exampleInputPassword1" class="form-label" >Password</label>
+            <input type="password" class="form-control" placeholder="Password" id="exampleInputPassword1">
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+
+    </div>
+    </div>
+</div>
+@endsection
